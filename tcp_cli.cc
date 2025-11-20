c75b7570ae72374569d144ed65fae6250644a595
FelixYuanSy <ysy12141314@gmail.com>
2025-11-21 00:44:16 +0800

    测试用cli

--- tcp_cli.cc
+++ tcp_cli.cc
@@ -1,64 +1,22 @@
 #include "server.hpp"
 
-void HandleRead(Channel channel)
-{
-    int fd = channel.Getfd();
-    char buf[1024] = {0};
-    int ret = recv(fd, buf, 1023, 0);
-    if (ret < 0)
-    {
-    }
-}
-
-std::unordered_map<uint64_t, PtrConnection> _conns;
-uint64_t conn_id = 0;
-void ConnectionDestroy(const PtrConnection &conn)
-{
-    _conns.erase(conn->GetId());
-}
-
-void OnConnected(const PtrConnection &conn)
-{
-    DBG_LOG("NEW CONNECTION:%p", conn.get());
-}
-
-void OnMessage(const PtrConnection &conn, Buffer *buf)
-{
-    DBG_LOG("%s", buf->ReadPosition());
-    buf->MoveReaderOffset(buf->ReadableSize());
-    std::string str = "Hello World";
-    conn->Send(str.c_str(), str.size());
-}
-
-void Acceptor(EventLoop *loop, Channel *lst_channel)
-{
-    int fd = lst_channel->Getfd();
-    int newfd = accept(fd, NULL, NULL);
-    if (newfd < 0)
-        return;
-    conn_id++;
-    PtrConnection conn(new Connection(loop, conn_id, newfd));
-    conn->SetMessageCallBack(std::bind(OnMessage, std::placeholders::_1, std::placeholders::_2));
-    conn->SetServeClosedCallBack(std::bind(ConnectionDestroy, std::placeholders::_1));
-    conn->SetConnectedCallBack(std::bind(OnConnected, std::placeholders::_1));
-    conn->EnableInactiveRelease(10);
-    conn->Established();
-    _conns.insert(std::make_pair(conn_id,conn));
-}
-
 int main()
 {
-    srand(time(NULL));
-    EventLoop loop;
-    Socket socket;
-    socket.CreateServer(8087);
-    Channel channel(&loop,socket.GetFd());
-    channel.SetReadCallBack(std::bind(Acceptor,&loop,&channel));
-    channel.EnableRead();
-    while(1)
+    Socket cli_sock;
+    cli_sock.CreateClient(8087, "127.0.0.1");
+    std::cout << "create succ"<<std::endl;
+    for (int i = 0; i < 5; i++)
     {
-        loop.Start();
+        std::string str = "hello bitejiuyeke!";
+        std::cout<<str<<std::endl;
+        cli_sock.Send(str.c_str(), str.size());
+        std::cout<<"Runing finish send"<<std::endl;
+        char buf[1024] = {0};
+        cli_sock.Rcev(buf, 1023);
+        DBG_LOG("%s", buf);
+        sleep(1);
     }
-    socket.Close();
+    while (1)
+        sleep(1);
     return 0;
 }
