16bc872d1d1e752ca0649609d15613e1e4056ba7
FelixYuanSy <ysy12141314@gmail.com>
2025-11-26 02:30:56 +0800

    测试url编码解码成功

--- test/test.cc
+++ test/test.cc
@@ -30,14 +30,81 @@ size_t Split(std::string &src, std::string sep, std::vector<std::string> *array)
     }
     return array->size();
 }
+static char HEXTOI(char c)
+{
+    if (c >= '0' && c <= '9')
+    {
+        return c - '0';
+    }
+    if (c >= 'a' && c <= 'z')
+    {
+        return c - 'a' + 10;
+    }
+    if (c >= 'A' && c <= 'Z')
+    {
+        return c - 'A' + 10;
+    }
+    return -1;
+}
+static std::string UrlDecode(const std::string url, bool convert_space_to_plus)
+{
+    std::string str;
+    for (int i = 0; i < url.size(); i++)
+    {
+        if (url[i] == '%' && (i + 2) < url.size())
+        {
+            char v1 = HEXTOI(url[i + 1]);
+            char v2 = HEXTOI(url[i + 2]);
+            char v = (v1 *16) + v2;
+            str += v;
+            i+=2;
+            continue;
+        }
+        if (url[i] == ' ' && convert_space_to_plus == true)
+        {
+            str += '+';
+            i += 2;
+            continue;
+        }
+        str += url[i];
+    }
+    return str;
+}
+static std::string UrlEncode(const std::string url, bool convert_space_to_plus)
+    {
+        std::string str;
+        for (auto &c : url)
+        {
+            if (c == '.' || c == '_' || c == '-' || c == '~' || isalnum(c))
+            {
+                str += c;
+                continue;
+            }
+            if (c == ' ' && convert_space_to_plus == true)
+            {
+                str += '+';
+                continue;
+            }
+            // 其余转化为%HH
+            char tmp[4];
+            snprintf(tmp, 4, "%%%02X", c); //%%用来转译为'%',第三个%用来表示后面为数字,0为未满两位数用0补齐第一位,2表示宽度为两位,X表示转换为16进制
+            str += tmp;
+        }
+        return str;
+    }
 
 int main()
 {
-    std::string str = "abc,,cba,dfd,vbn,";
-    std::vector<std::string> array;
-    Split(str, ",", &array);
-    for (auto &s : array)
-    {
-        std::cout << "[" << s << "]\n";
-    }
+    // std::string str = "abc,,cba,dfd,vbn,";
+    // std::vector<std::string> array;
+    // Split(str, ",", &array);
+    // for (auto &s : array)
+    // {
+    //     std::cout << "[" << s << "]\n";
+    // }
+    std::string str = "c++";
+    std::string eco = UrlEncode(str,false);
+    std::string res = UrlDecode(eco, false);
+    std::cout << eco << std::endl;
+    std::cout << res << std::endl;
 }
