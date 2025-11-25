66c1ee3f20cf26d0fe91208a99cbb854de6a323f
FelixYuanSy <ysy12141314@gmail.com>
2025-11-25 14:36:30 +0800

    增加Util工具类里的Split

---
+++ http/util.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include <iostream>
+#include <vector>
+class util
+{
+    public:
+    size_t Split(std::string &src,std::string sep,std::vector<std::string> *array)
+    {
+        //情况1: abc
+        //情况2: abc,,cba
+        //情况3: ab,cdd,dd
+        //情况4: ,abc
+        //情况5: abc,
+
+        size_t offset = 0;
+        while(offset < src.size())
+        {
+            size_t pos = src.find(sep,offset);
+            if(pos == src.size())
+                break;
+            if(pos == std::string::npos)
+            {
+                array->push_back(src.substr(offset));
+                return array->size();
+            }
+            if(offset == pos)
+            {
+                offset = pos + sep.size();
+                continue;
+            }
+            array->push_back(src.substr(offset,pos-offset));
+            offset = pos + sep.size();
+        }
+        return array->size();
+    }
+};
